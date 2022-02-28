1f442f54903bd700bb03b1edf3d0f921ef1c9e3c
Sakib Rahman <seu.sakib@gmail.com>
2022-02-28 23:16:40 +0600

    Add function -> clear Search Box

--- js/app.js
+++ js/app.js
@@ -1,3 +1,9 @@
+// clear Search Box
+const clearSearchBox = () => {
+  const searchFeild = document.getElementById("search-box");
+  searchFeild.value = "";
+};
+
 // search button function
 const searchBtn = () => {
   const searchValue = document.getElementById("search-box").value;
